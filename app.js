285ae8142fb30e6bf56f2b8e7446bd126f3d20ae
Veronica Lobkina <95447175+verosfbay@users.noreply.github.com>
2022-06-12 16:48:18 -0700

    updates

--- app.js
+++ app.js
@@ -57,18 +57,20 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(filters).forEach(([key,value])=> {
+      filteredData =  filteredData.filter(row => row[key]=== value);
+    });  
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("input").on("change",updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
